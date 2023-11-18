093dd64448c4b6111b4e367173edff79200bdf53
DocTzz <151367880+DocTzz@users.noreply.github.com>
2023-11-18 15:40:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+///Edit Under This only
+
+
+    var firebaseConfig = {
+    apiKey: "AIzaSyBq2nsqBWarsplWMfCdqR95Fxa5jVbKXew",
+    authDomain: "dork-b331b.firebaseapp.com",
+    databaseURL: "https://dork-b331b-default-rtdb.firebaseio.com",
+    projectId: "dork-b331b",
+    storageBucket: "dork-b331b.appspot.com",
+    messagingSenderId: "31645285587",
+    appId: "1:31645285587:android:4d6451b947fbc1e334e826"
+  }; 
+ 
+////Do Not Edit Below This
+
+firebase.initializeApp(firebaseConfig);
